f14a0b7c3ce93d27560556e0ad10b1bc13d8b3e6
Olha36 <olhakucheruk36@gmail.com>
2024-11-23 20:18:56 +0100

    add pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <h1>Home</h1>
+      <h2>error</h2>
+    </div>
+  )
+}
+
+export default Home

8f42999afbf01790919ae2e0879fdb64e98bda0c
Olha36 <olhakucheruk36@gmail.com>
2024-11-24 16:14:58 +0100

    add functionality for show students button

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,28 +1,44 @@
+import { useState } from 'react';
 import '../css/welcome.css';
+import houses from '../img/houses.jpg';
 import students from '../img/students.jpg';
 import teachers from '../img/teachers.jpg';
-import houses from '../img/houses.jpg';
+
+interface Button {
+  (): void;
+}
 
 function Welcome() {
+  const [showCharacters, setShowCharacters] = useState(false);
+  const [text, setText] = useState(false);
+
+  const handleShowCharactersButton: Button = () => {
+    setShowCharacters((prev: boolean) => !prev);
+    setText((prev: boolean) => !prev);
+  };
+
   return (
     <div className='welcome-wrapper'>
       <div className='welcome-container'>
         <h1>
           Ласкаво просимо <br /> у світ Гаррі Поттера
         </h1>
-        <button className='show-characters'>Показати всіх персонажів</button>
+        <button className='show-characters' onClick={handleShowCharactersButton}>
+          {' '}
+          {text ? 'Сховати персонажів' : 'Показати всіх персонажів'}
+        </button>
       </div>
-      <div className="welcome-cards">
-        <div className="welcome-students">
-          <img src={students} alt="students" />
+      <div className='welcome-cards' style={{ display: showCharacters ? 'flex' : 'none' }}>
+        <div className='welcome-students'>
+          <img src={students} alt='students' />
           <button>Студенти Хогвортсу</button>
         </div>
-        <div className="welcome-teachers">
-          <img src={teachers} alt="teachers" />
+        <div className='welcome-teachers'>
+          <img src={teachers} alt='teachers' />
           <button>Співробітники Хогвордсу</button>
         </div>
-        <div className="welcome-houses">
-          <img src={houses} alt="houses" />
+        <div className='welcome-houses'>
+          <img src={houses} alt='houses' />
           <button>Персонажі в певному будинку</button>
         </div>
       </div>

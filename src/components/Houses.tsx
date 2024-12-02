754dd0b726550b5dfda35a396b83112bcd9c90ae
Olha36 <olhakucheruk36@gmail.com>
2024-12-02 10:44:45 +0100

    add house alternate_names

--- src/components/Houses.tsx
+++ src/components/Houses.tsx
@@ -25,7 +25,19 @@ function Houses() {
   interface House {
     id: string;
     name: string;
+    alternate_names: string[];
+    species: string;
+    gender: string;
     house: string;
+    dateOfBirth: string;
+    wizard: boolean;
+    ancestry: string;
+    eyeColour: string;
+    hairColour: string;
+    patronus: string;
+    hogwartsStudent: boolean;
+    hogwartsStaff: boolean;
+    actor: string;
     image: string;
   }
 
@@ -36,31 +48,29 @@ function Houses() {
     Hufflepuff: 'https://hp-api.onrender.com/api/characters/house/hufflepuff',
   };
 
+  const fetchHouseData = async (houseName: string) => {
+    const url = houseAPIs[houseName];
+    setError(null);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      setHouses(data);
+      setSelectedHouse(houseName);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      }
+    }
+  };
 
-    const fetchHouseData = async (houseName: string) => {
-        const url = houseAPIs[houseName];
-        setError(null);
-        try {
-          const response = await fetch(url);
-          if (!response.ok) {
-            throw new Error(`Error: ${response.status}`);
-          }
-          const data = await response.json();
-          setHouses(data);
-          setSelectedHouse(houseName);
-        } catch (error) {
-          if (error instanceof Error) {
-            setError(error.message);
-          }
-        }
-      };
+  const handleHouseClick = (houseName: string) => {
+    console.log(houseName);
+    fetchHouseData(houseName);
+  };
 
-      const handleHouseClick = (houseName: string) => {
-        console.log(houseName);
-        fetchHouseData(houseName);
-      };
-
-      
   return (
     <>
       <div className='hogwards-wrapper'>
@@ -71,7 +81,11 @@ function Houses() {
       </div>
       {error && <p style={{ color: 'red' }}>{error}</p>}
       <div className='houses-list flex'>
-        <div className='gryffindor flex align-items active' role='button' onClick={() => handleHouseClick('Gryffindor')}>
+        <div
+          className='gryffindor flex align-items active'
+          role='button'
+          onClick={() => handleHouseClick('Gryffindor')}
+        >
           <img src={GryffindorImg} alt='Gryffindor' className='house-img' />
           <h3 className='house-name'>Gryffindor</h3>
         </div>
@@ -89,13 +103,49 @@ function Houses() {
         </div>
       </div>
       {selectedHouse && (
-        <div className='house-details'>
+        <div className='house-details '>
           <h2>{selectedHouse} Characters</h2>
-          <div className='house-characters'>
+          <div className='house-characters container'>
             {houses.map((house) => (
-              <div key={house.id} className='house-card'>
-                <img src={house.image} alt={house.name} className='character-img' />
-                <h4>{house.name}</h4>
+              <div
+                key={house.id}
+                className='house-card container-card'
+                style={{
+                  backgroundImage: house.image ? `url(${house.image})` : 'none',
+                  backgroundSize: house.image ? 'cover' : 'initial',
+                  backgroundPosition: house.image ? 'center' : 'initial',
+                }}
+              >
+                {!house.image && (
+                  <div
+                    style={{
+                      height: '100%',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <span style={{ fontSize: '25px', fontStyle: 'italic' }}>Image is absent in the URL</span>
+                  </div>
+                )}
+                {/* <img src={house.image} alt={house.name} className='character-img' /> */}
+                <div className='card-content'>
+                  <h4>{house.name}</h4>
+                  <p>
+                    {house.alternate_names && house.alternate_names.length > 0 ? (
+                      house.alternate_names.slice(0, 2).map((name, index) => (
+                        <span key={index}>
+                          {name}
+                          {index === 0 && house.alternate_names.length > 1 ? ',' : ''}
+                        </span>
+                      ))
+                    ) : (
+                      <span>Character has no alternative names</span>
+                    )}
+                  </p>
+
+                  <p>{house.gender}</p>
+                </div>
               </div>
             ))}
           </div>
